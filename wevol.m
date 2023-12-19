aba2f63bb229265112256cbdc2f9b660591d9340
Christian Tezza <65645957+tezzachris@users.noreply.github.com>
2023-12-19 12:01:57 +0100

    Update wevol.m

--- wevol.m
+++ wevol.m
@@ -1,5 +1,8 @@
-function ll = wevol(w,r)
-    %w vettore riga lunghezza 13
+%Inputs:
+%w:  13 x 1 double  i.e. weights of 12 assets + cash
+%r:  n x 13 double  i.e. returns vector
+
+function [SR_annual] = weights_evolution(w,r)
     f = 5e-5;
     Rt = zeros(size(r,1),1); 
     for i = 1:size(r,1)
@@ -7,9 +10,9 @@ function ll = wevol(w,r)
              %we assume here f = 0;
              Rt(i) =  w(i,1:end-1)*r(i,:)';
         elseif i > 1
-        cost = f*sum( abs( ( w(i-1,1:end-1) .* (1+r(i,:)) )/(1+w(i,1:end-1)*r(i,:)') - w(i,1:end-1)) ) ;
-        Rt(i) =  w(i,1:end-1)*r(i,:)' - cost ;
+            cost = f*sum( abs( ( w(i-1,1:end-1) .* (1+r(i,:)) )/(1+w(i,1:end-1)*r(i,:)') - w(i,1:end-1)) ) ;
+            Rt(i) =  w(i,1:end-1)*r(i,:)' - cost ;
         end
     end
-    ll = - mean(Rt)/std(Rt); %*sqrt(365*24)
+    SR_annual = - mean(Rt)/std(Rt) * sqrt(365*24) %note the minus sign, we need for the optmisation
 end
