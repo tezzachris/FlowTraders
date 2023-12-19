6d671f7c4fcdeed03701cd90b7a53a07349013f3
Christian Tezza <65645957+tezzachris@users.noreply.github.com>
2023-12-19 12:15:19 +0100

    Update weights_evolution.m

--- weights_evolution.m
+++ weights_evolution.m
@@ -10,8 +10,13 @@ function [SR_annual] = weights_evolution(w,r)
              %we assume here f = 0;
              Rt(i) =  w(i,1:end-1)*r(i,:)';
         elseif i > 1
+            
+            %we take the given cost function
             cost = f*sum( abs( ( w(i-1,1:end-1) .* (1+r(i,:)) )/(1+w(i,1:end-1)*r(i,:)') - w(i,1:end-1)) ) ;
+            
+            %we take the given return function
             Rt(i) =  w(i,1:end-1)*r(i,:)' - cost ;
+            
         end
     end
     SR_annual = - mean(Rt)/std(Rt) * sqrt(365*24) %note the minus sign, we need for the optmisation
